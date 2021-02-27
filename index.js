b4f900bcf8ab3670634410a51652ed752c0ecc21
Fabricio Sousa <fabricio1234@gmail.com>
2021-02-27 18:31:35 -0300

    tamplete string

---
+++ index.js
@@ -0,0 +1,12 @@
+const nome = 'Fabricio';
+const sobreNome = 'Sousa';
+const altura = 1.71;
+const idade = 22;
+const peso = 65;
+
+let imc;
+imc = peso / (altura * altura);
+let anoNascimento ;
+
+
+console.log(`${nome} ${sobreNome} tem ${idade} anos e pesa ${peso + ' quilos'} tem ${altura} de altura e seu IMC é de  ${imc}`)
